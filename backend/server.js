0d356b8afd54f924881a8da08ba628f4c78728f8
Bora Yetkin <borayetkin@gmail.com>
2024-10-15 13:21:14 +0300

    Some folder changes

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,3 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const connectDB = require("./config/db");
-// const authRoutes = require("./routes/authRoutes");
-// const path = require("path");
-
-// // Load environment variables
-// dotenv.config();
-
-// // Connect to MongoDB
-// connectDB();
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Serve static files from the 'public' folder
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // API Routes
-// app.use("/api/auth", authRoutes);
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
