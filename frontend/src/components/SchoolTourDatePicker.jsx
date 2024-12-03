1311b1db329ac18e99adaef800f2ea5c83d15c9f
Tunagoksal <tuna.goksal@ug.bilkent.edu.tr>
2024-12-03 14:48:11 +0300

    little fix

--- frontend/src/components/SchoolTourDatePicker.jsx
+++ frontend/src/components/SchoolTourDatePicker.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "react-datepicker/dist/react-datepicker.css";
 import "../styles/CustomDatePicker.css"
 import "../styles/TourApplication.css";
 import DatePicker2 from "./DatePicker2";
